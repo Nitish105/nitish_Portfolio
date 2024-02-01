f5127b53327b8fbb02745a1ea4b45019108b4817
Nitish Kumar <83354680+Nitish105@users.noreply.github.com>
2024-02-02 03:19:47 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const myproject = [
     {
       id: 1,
-      img: "https://i.ibb.co/4TD6cYF/1.jpg",
+      img: "https://private-user-images.githubusercontent.com/83354680/297513509-22e7403d-c49d-4136-a6aa-cdfcb0d63a12.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY4MjQzODgsIm5iZiI6MTcwNjgyNDA4OCwicGF0aCI6Ii84MzM1NDY4MC8yOTc1MTM1MDktMjJlNzQwM2QtYzQ5ZC00MTM2LWE2YWEtY2RmY2IwZDYzYTEyLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAyMDElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMjAxVDIxNDgwOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWExNjg0NTc4NmY0MjQ1YzZlNTJjOTkyMmYwM2I2ZDcyNjg4OGIxY2EwZGZkYzEzZDEwMDdkNjBjOGFjZGZkNWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.k-H4HInxF1gNqsA2NZqAdf97FvQVRFMqgsT4yp9AGWs",
       link: "https://emart-dynamic-ecommerce-app.onrender.com/",
     },
     {
